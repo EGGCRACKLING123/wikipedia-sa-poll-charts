5d89702189d7ed5e5eb1d3930f52c159c9b43bb8
EGGCRACKLING123 <TheHelpfulGamer88@gmail.com>
2025-04-16 17:58:08 +1000

    Update and rename polling-graph-weighted (1).R to polling-graph-weighted.R

--- polling-graph-weighted.R
+++ polling-graph-weighted.R
@@ -1,13 +1,13 @@
 library(ggplot2)
 library(dplyr)
 library(lubridate)
-setwd("~/Documents/GitHub/wikipedia-vic-poll-charts") # replace with your own working directory
+setwd("~/Documents/GitHub/wikipedia-sa-poll-charts") # replace with your own working directory
 polling2226 <- read.csv("polling2226.csv")
 spansize <- 0.6
 
-election22 <- data.frame(c("LNP","ALP","GRN","OTH"), c(34.4,37.0,11.5,17.1))
+election22 <- data.frame(c("LNP","ALP","GRN","OTH"), c(35.67,39.97,9.12,15.64))
 #election25 <- data.frame(c("LNP","ALP","GRN","OTH"), c())
-election22tpp <- data.frame(c("LNP","ALP"), c(45.0,55.0))
+election22tpp <- data.frame(c("LNP","ALP"), c(45.10,54.59))
 #election25tpp <- data.frame(c("LNP","ALP"), c())
 names(election22) <- c("party", "vote")
 #names(election25) <- c("party", "vote")
@@ -33,12 +33,12 @@ primary_votes <- ggplot(polling2226, aes(x=as.Date(last_date, '%d %b %Y'))) +
   geom_smooth(aes(y=pv_grn, colour="GRN", weight=sqrt(sample_size)), span = spansize, se = FALSE) +
   geom_point(aes(y=pv_oth, size=sample_size), colour="grey60", alpha = 3/10) +
   geom_smooth(aes(y=pv_oth, colour="OTH", weight=sqrt(sample_size)), span = spansize, se = FALSE) +
-  geom_point(data = election22, aes(x = as.Date('2022-11-26', '%Y-%m-%d'), y = vote, colour = party), shape=23, stroke=0.5, fill = "#FFFFFF", size=4) +
-  geom_point(data = election22, aes(x = as.Date('2022-11-26', '%Y-%m-%d'), y = vote, colour = party), shape=18, size=3) +
+  geom_point(data = election22, aes(x = as.Date('2022-03-19', '%Y-%m-%d'), y = vote, colour = party), shape=23, stroke=0.5, fill = "#FFFFFF", size=4) +
+  geom_point(data = election22, aes(x = as.Date('2022-03-19', '%Y-%m-%d'), y = vote, colour = party), shape=18, size=3) +
   #geom_point(data = election25, aes(x = as.Date('2025-05-??', '%Y-%m-%d'), y = vote, colour = party), shape=23, stroke=0.5, fill = "#FFFFFF", size=4) +
   #geom_point(data = election25, aes(x = as.Date('2025-05-??', '%Y-%m-%d'), y = vote, colour = party), shape=18, size=3) +
   scale_y_continuous(limits=c(0, 50), breaks=c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50), minor_breaks = NULL, expand = c(0,0)) +
-  scale_x_date(limits=c(as.Date('2022-11-26', '%Y-%m-%d'), max_date), date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = "1 month", expand = c(0,0)) +
+  scale_x_date(limits=c(as.Date('2022-03-19', '%Y-%m-%d'), max_date), date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = "1 month", expand = c(0,0)) +
   guides(colour = guide_legend(override.aes = list(alpha = 1, shape = 16, size = 3))) +
   theme(legend.key = element_rect(colour = NA, fill = NA), legend.text=element_text(size=12), axis.text.y = element_text(size=12), axis.text.x = element_text(angle=45, vjust=0.5, size=12)) +
   labs(y="Voters (%)", x= NULL) +
@@ -56,12 +56,12 @@ tpp <- ggplot(polling2226, aes(x=as.Date(last_date, '%d %b %Y'))) +
   geom_smooth(aes(y=tpp_lnp, colour="LNP", weight=sqrt(sample_size)), span = spansize, se = FALSE) +
   geom_point(aes(y=tpp_alp, size=sample_size), colour="red3", alpha = 3/10) +
   geom_smooth(aes(y=tpp_alp, colour="ALP", weight=sqrt(sample_size)), span = spansize, se = FALSE) +
-  geom_point(data = election22tpp, aes(x = as.Date('2022-11-26', '%Y-%m-%d'), y = vote, colour = party), shape=23, stroke=0.5, fill = "#FFFFFF", size=4) +
-  geom_point(data = election22tpp, aes(x = as.Date('2022-11-26', '%Y-%m-%d'), y = vote, colour = party), shape=18, size=3) +
+  geom_point(data = election22tpp, aes(x = as.Date('2022-03-19', '%Y-%m-%d'), y = vote, colour = party), shape=23, stroke=0.5, fill = "#FFFFFF", size=4) +
+  geom_point(data = election22tpp, aes(x = as.Date('2022-03-19', '%Y-%m-%d'), y = vote, colour = party), shape=18, size=3) +
   #geom_point(data = election25tpp, aes(x = as.Date('2025-05-??', '%Y-%m-%d'), y = vote, colour = party), shape=23, stroke=0.5, fill = "#FFFFFF", size=4) +
   #geom_point(data = election25tpp, aes(x = as.Date('2025-05-??', '%Y-%m-%d'), y = vote, colour = party), shape=18, size=3) +
   scale_y_continuous(limits=c(34, 65), breaks=c(40,45,50,55,60), minor_breaks = NULL, expand = c(0,0)) +
-  scale_x_date(limits=c(as.Date('2022-11-26', '%Y-%m-%d'), max_date), date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = "1 month", expand = c(0,0)) +
+  scale_x_date(limits=c(as.Date('2022-03-19', '%Y-%m-%d'), max_date), date_breaks = "1 month", date_labels = "%b %Y", minor_breaks = "1 month", expand = c(0,0)) +
   theme(legend.key = element_rect(colour = NA, fill = NA), legend.text=element_text(size=12), axis.text.y = element_text(size=12), axis.text.x = element_text(angle=45, vjust=0.5, size=12)) +
   labs(y="Voters (%)", x= NULL) +
   scale_colour_manual(name="", 
